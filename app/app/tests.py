4c7557871f75e2ca342eba735ff76c1c37be613b
Balamithran228 <ssbalabhavani228@gmail.com>
2024-02-17 01:58:47 +0530

    add admin part and user model and UIswagger 1

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+
+from django.test import SimpleTestCase
+from .calc import add
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
